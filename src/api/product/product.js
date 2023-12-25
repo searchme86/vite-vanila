77f49d3952e2046aa60d44cc94d69b289bd6f938
reviewer <reviewer@review.view>
2023-12-25 16:56:18 +0900

    fix(api): rename file, add api/product folder

--- src/api/product/product.js
+++ src/api/product/product.js
@@ -2,7 +2,17 @@
  * 페이지, 상품 페이지 및 단일 상품의 아이템 정보를 fetch 하는 스크립트
  */
 
-const FetchProductsApi = "https://dummyjson.com/products";
+
+// TODO: env 에 추가 필요
+const DEFAULT_PATH = 'https://dummyjson.com/';
+
+// TODO: 단일 get, list get의 naming은 어떻게 하는지 숙지 필요.
+export const api = {
+    product: {
+        list: `${DEFAULT_PATH}/products`,
+        one: `${DEFAULT_PATH}/products`,
+    }
+}
 
 export const fetchProducts = async () => {
   try {
