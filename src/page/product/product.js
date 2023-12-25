6486870253e508226c4080779217792391fa5b12
reviewer <reviewer@review.view>
2023-12-25 19:24:54 +0900

    fix(product): refactor product.js

--- src/page/product/product.js
+++ src/page/product/product.js
@@ -19,6 +19,11 @@ import {
   rendererTable,
 } from "./renderer";
 
+const description = document.querySelector(".box__info-description");
+const itemTable = document.querySelector(".box__item-table");
+const productArrange = document.querySelector(".box__item-arrange");
+const cartButton = document.querySelector(".box__item-detail");
+
 const init = async () => {
   const match = window.location.search.match(/\d+/);
   if (!match) {
@@ -26,34 +31,26 @@ const init = async () => {
   }
 
   const result = parseInt(match[0], 10);
-
   try {
-    const some = document.querySelector(".box__info-description");
-    const itemTable = document.querySelector(".box__item-table");
-
     const item = await getProduct(result);
-
     const itemDetailDOM = rendererDetailItem(item);
-    document.querySelector(".box__item-arrange").innerHTML = itemDetailDOM;
+    productArrange.innerHTML = itemDetailDOM;
 
-    const imageList = item.images;
-
-    if (imageList.length > 3) {
-      some.innerHTML = rendererInfoDescription(item);
+    if (item.images.length > 3) {
+      description.innerHTML = rendererInfoDescription(item);
     }
-    if (imageList.length === 3) {
-      some.innerHTML = rendererInfoTDescription(item);
+    if (item.images.length === 3) {
+      description.innerHTML = rendererInfoTDescription(item);
     }
-
-    if (imageList.length === 1) {
-      some.innerHTML = rendererInfoSingleDescription(item);
+    if (item.images.length === 1) {
+      description.innerHTML = rendererInfoSingleDescription(item);
     }
 
     itemTable.innerHTML = rendererTable(item);
 
-    const cartButton = document.querySelector(".box__item-detail");
-
+    // TODO 이거 쓰이는 코드인지 확인필요
     cartButton.querySelector(".button__add-item");
+
     cartButton.addEventListener("click", function (e) {
       const dataId = e.target.dataset.id;
       if (dataId) {
