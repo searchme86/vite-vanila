6d04e46fe72d7df338304aba90b8b6497ddf90ff
reviewer <reviewer@review.view>
2023-12-25 18:59:58 +0900

    fix(productList): refactor toggle.js

--- src/page/productList/component/toggle.js
+++ src/page/productList/component/toggle.js
@@ -1,21 +1,12 @@
-/**
- * 상품 페이지(products.html)에서 카드 상품에 대한 스크립트
- */
-
-import {
-  cartOverlay,
-} from "../../util/cartElemDom.js";
-
-import {slider} from "./render.js";
+import { slider } from "./render.js";
 
 const modalLayer = document.querySelector(".box__modal-layer");
 const cartOverlay = document.querySelector(".box__modal-layer");
 // TODO 이미 모달은 여기에서 찾고있는데 버튼도 한곳에서만 쓰이고 있기 때문에 util이 아님, 그리고 그정도의 헤비함이 없음. 분리 불필요함.
 // TODO 쓰일것 같으니까 미리 빼는게 아님. 쓰다보니까 중복코드가 생길때 빼는거임.
-const closeCartBtn = document.querySelector(".button__close-layer");
-const toggleCartBtn = document.querySelector(".button__item-cart");
+const closeButton = document.querySelector(".button__close-layer");
+const toggleButton = document.querySelector(".button__item-cart");
 
-// 상품의 카트 버튼을 클릭 시, 페이지 하단 모달을 열어주는 함수
 const showCartOverlay = () => {
   if (modalLayer) {
     cartOverlay.classList.add("show");
@@ -23,16 +14,15 @@ const showCartOverlay = () => {
   slider.init(document.querySelector(".swiper"));
 };
 
-// 모달을 제거하는 함수
 const hideCartOverlay = () => {
   if (modalLayer) {
     cartOverlay.classList.remove("show");
   }
 };
 
-if (closeCartBtn) {
-  toggleCartBtn.addEventListener("click", showCartOverlay);
-  closeCartBtn.addEventListener("click", hideCartOverlay);
+if (closeButton) {
+  toggleButton.addEventListener("click", showCartOverlay);
+  closeButton.addEventListener("click", hideCartOverlay);
 }
 
 export const openCart = showCartOverlay;
