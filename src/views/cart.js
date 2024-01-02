95f0d7a99d12eb2678971652c3a0f44a5becf71e
searchme86 <searchme86@gmail.com>
2024-01-02 18:32:41 +0900

    adding dist

--- src/views/cart.js
+++
@@ -1,6 +0,0 @@
-import "../assets/style/components/header.scss";
-import "../assets/style/components/footer.scss";
-import "../assets/style/media/headerMedia.scss";
-import "../assets/style/pages/cart.scss";
-
-import "../components/bannerSlider";
